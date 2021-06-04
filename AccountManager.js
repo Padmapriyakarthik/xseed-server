995349507a1478148ce753845435abae73cc5af8
PadmapriyaKarthik <padmapriyakarthik97@gmail.com>
2021-06-04 15:42:46 +0530

    redirection link

--- AccountManager.js
+++ AccountManager.js
@@ -51,7 +51,7 @@ const activateToken=async(req,res)=>{
                    const user=await confirmUser(decode.email);
                     if(user)
                     {
-                     res.redirect(baseurl+"/redirectlogin");
+                     res.redirect("https://xseed-client.herokuapp.com/redirectlogin");
                     }          
                 }else{
                     res.status(401).json({message:"invalid token"});
